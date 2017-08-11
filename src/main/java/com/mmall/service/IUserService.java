afcd4efba51c141b8228930681264e7dcef19752
wangzehuai <874697675@qq.com>
2017-08-11 20:55:03 +0800

    first commit

---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,20 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+import java.util.List;
+
+/**
+  * @Author: wzh
+  * @Description:
+  * @Date: Created in 10:29 2017/7/31
+  */
+public interface IUserService {
+    ServerResponse<User> login(String username, String password);
+    ServerResponse<String> register(User user);
+    ServerResponse<String> checkValid(String str, String type);
+    ServerResponse selectQuestion(String username);
+    ServerResponse<String> checkAnswer(String username,String question,String answer);
+    ServerResponse<String> forgetRestPassword(String username,String passwordNew,String forgetToken);
+}
