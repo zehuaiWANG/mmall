4b97e1613dff40033deb1042e1906a8f324fedd9
wangzehuai <874697675@qq.com>
2017-08-15 10:04:46 +0800

    the 7th commit

--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -170,4 +170,15 @@ public class UserServiceImpl implements IUserService {
         return ServerResponse.createBySuccess(user);
 
     }
+
+    //backend
+    /*
+    * 校验是否为管理员
+    * */
+    public ServerResponse checkAdminRole(User user){
+        if (user != null && user.getRole().intValue() == Const.Role.ROLE_ADMIN){
+            return ServerResponse.createBySuccess();
+        }
+        return ServerResponse.createByError();
+    }
 }
